a0f96048457a4341f0ebc8745e3b68ba92765776
Steve Lau <liuwenhong_steve@163.com>
2018-01-22 15:21:51 +0800

    Create operator.py

---
+++ pythonExercise/operator.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+print(1+1)
+
+print(1-1)
+
+print(2*2)
+
+print(3/4)
+
+print(3//4)
+
+print(3**2)
+
+print(10%4)
+
+print(1==1)
+
+print(1!=1)
+
+print(1==True)
+
+print(1==False)
+
+print(1<2)
+
+print(1>3)
+
+print(1<=5)
+
+print(1>=0)
+
+print(1 in [1,3,5,6])
+
+print(1 and 0)
+
+print(1 or 0)
+
+print(True and False)
+
+print(True or False)
+
+print(not False)
+
+print(not True)
+
+print(not 1==1)
+
+print(not 1==0)
+
+print(not 1==-1)
+
+a = 20
+
+b = 20
+
+print(a is b)
